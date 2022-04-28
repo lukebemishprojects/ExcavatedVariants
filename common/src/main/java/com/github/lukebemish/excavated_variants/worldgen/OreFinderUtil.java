67a3dd2e7f114f345c19ab2174eab332f93faa4a
Luke Bemish <lukebemish@gmail.com>
2022-04-28 14:57:47 -0500

    Update OreFinderUtil.java

--- common/src/main/java/com/github/lukebemish/excavated_variants/worldgen/OreFinderUtil.java
+++ common/src/main/java/com/github/lukebemish/excavated_variants/worldgen/OreFinderUtil.java
@@ -9,25 +9,22 @@ import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class OreFinderUtil {
-    private static Map<ResourceLocation, Pair<BaseOre,List<BaseStone>>> lookupMap;
-
-    private static void reset() {
-        lookupMap = null;
-    }
 
     public static void setupBlocks() {
-        reset();
         for (Block block : Registry.BLOCK) {
-            ((IOreFound)block).excavated_variants$set_pair(getBaseOre(block));
+            ((IOreFound)block).excavated_variants$set_pair(null);
             ((IOreFound) block).excavated_variants$set_stone(null);
             if (ExcavatedVariants.setupMap()) {
                 for (Pair<BaseOre, List<BaseStone>> p : ExcavatedVariants.oreStoneList) {
+                    for (ResourceLocation rl : p.first().block_id) {
+                        Block bl2 = RegistryUtil.getBlockById(rl);
+                        if (bl2==block) {
+                            ((IOreFound)block).excavated_variants$set_pair(p);
+                        }
+                    }
                     for (BaseStone stone : p.last()) {
                         if (stone.block_id.equals(RegistryUtil.getRlByBlock(block))) {
                             ((IOreFound) block).excavated_variants$set_stone(stone);
@@ -36,38 +33,5 @@ public class OreFinderUtil {
                 }
             }
         }
-    }
-
-    private static Pair<BaseOre, List<BaseStone>> getBaseOre(Block block2) {
-        if (!ExcavatedVariants.setupMap()) {
-            return null;
-        }
-        if (lookupMap == null) {
-            lookupMap = new HashMap<>();
-            for (Pair<BaseOre, List<BaseStone>> pair : ExcavatedVariants.oreStoneList) {
-                ArrayList<ResourceLocation> pairedBlocks = new ArrayList<>();
-                for (ResourceLocation rl : pair.first().block_id) {
-                    Block block = RegistryUtil.getBlockById(rl);
-                    if (block != null) {
-                        pairedBlocks.add(rl);
-                    }
-                }
-                for (BaseStone stone : pair.last()) {
-                    ResourceLocation rl = new ResourceLocation(ExcavatedVariants.MOD_ID,stone.id+"_"+pair.first().id);
-                    Block block = RegistryUtil.getBlockById(rl);
-                    if (block != null) {
-                        pairedBlocks.add(rl);
-                    }
-                }
-                for (ResourceLocation rl : pairedBlocks) {
-                    lookupMap.put(rl, pair);
-                }
-            }
-        }
-        ResourceLocation testing = RegistryUtil.getRlByBlock(block2);
-        if (lookupMap.containsKey(testing)) {
-            return lookupMap.get(testing);
-        }
-        return null;
     }
 }
