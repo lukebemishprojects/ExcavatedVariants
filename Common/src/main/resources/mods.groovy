45e38b68f25033c3aec698d7894269f5c9901492
Luke Bemish <lukebemish@lukebemish.dev>
2023-04-24 22:01:52 -0500

    CHORE: Update publishing and add license headers

--- Common/src/main/resources/mods.groovy
+++ Common/src/main/resources/mods.groovy
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2023 Luke Bemish and contributors
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
 ModsDotGroovy.make {
     modLoader = 'javafml'
     loaderVersion = '[40,)'
